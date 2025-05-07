c3d7b271465a0d143bb2c25dde98be5a11b49dc2
Rybis02 <rabischawarri@gmail.com>
2025-05-07 14:50:13 +0200

    added login and loading of blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,85 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import Blog from './components/Blog';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const initialNotes = await blogService.getAll();
+        setBlogs(initialNotes);
+      } catch (error) {
+        console.error('Failed to fetch blogs:', error);
+      }
+    };
+
+    fetchBlogs();
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+
+  if (!user) {
+    return (
+      <div>
+        <LoginForm
+          username={username}
+          setUsername={setUsername}
+          handleLogin={handleLogin}
+          password={password}
+          setPassword={setPassword}
+        />
+      </div>
+    );
+  }
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Blogs</h1>
+        <h2>Logged in as {user.name}</h2>
       </div>
-      <h1>Vite+ React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <ul>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
