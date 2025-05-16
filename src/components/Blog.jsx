a2affc67b761cad2ddfccadbf68b33ffd41fa29d
Rybis02 <rabischawarri@gmail.com>
2025-05-16 14:50:53 +0200

    added functionality for expanding view

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,11 +1,49 @@
+import { useState } from 'react';
+
 const Blog = ({ blog }) => {
-  return (
-    <li className="blog">
-      <em>Title</em>: <strong>{blog.title}</strong> | <em>Author:</em>{' '}
-      <strong>{blog.author}</strong> | <em>Likes:</em>{' '}
-      <strong>{blog.likes}</strong>
-    </li>
-  );
+  const [view, setView] = useState(false);
+
+  if (!view) {
+    return (
+      <div className="blog">
+        <li>
+          <strong>{blog.title}</strong>
+          <button className="view" onClick={() => setView(true)}>
+            expand
+          </button>
+        </li>
+        <hr />
+      </div>
+    );
+  }
+
+  if (view) {
+    return (
+      <div className="blog">
+        <li>
+          <ul className="expand">
+            <li>
+              <em>Title</em>: <strong>{blog.title}</strong>
+            </li>
+            <li>
+              <em>url:</em> <strong>{blog.url}</strong>
+            </li>
+            <li>
+              <em>Author:</em> <strong>{blog.author}</strong>
+            </li>
+            <li>
+              <em>Likes:</em> <strong>{blog.likes}</strong>{' '}
+              <button className="like">like</button>
+            </li>
+          </ul>
+          <button className="hide" onClick={() => setView(false)}>
+            hide
+          </button>
+        </li>
+        <hr />
+      </div>
+    );
+  }
 };
 
 export default Blog;
